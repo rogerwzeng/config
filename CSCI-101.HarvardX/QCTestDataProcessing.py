413934d8038cbd7f079823e7a17f6ab257a2e18f
Roger Zeng <roger@cora.org.cn>
2022-07-06 23:23:21 +0800

    Pandas workshop #1

--- CSCI-101.HarvardX/QCTestDataProcessing.py
+++ CSCI-101.HarvardX/QCTestDataProcessing.py
@@ -10,7 +10,18 @@ import QCTestString as qt
 
 df = pd.read_excel('QCTest.xlsx', sheet_name="Sheet1", index_col=None,na_values="NA", header=0)
 
-# Add the validity of the QC test string for each plant
-df['isValid'] = df['TestResults'].apply(lambda x: 'yes' if qt.isValidString(str(x)) == True else 'no')
+# Validate the QC test string for each plant and add stats cols
+ds = df['TestResults'].apply(lambda x: qt.isValidString(str(x))).to_frame()
+# df[['isValid', 'TotalTests', 'Pass', 'Defect']] = df['TestResults'].apply(lambda x: qt.isValidString(str(x))).to_frame()
 
+# Convert to Dataframe
+dfa = pd.DataFrame(ds['TestResults'].tolist(), columns=['isValid', 'TotalTests', 'Pass','Defect'])
+
+# Adjust status col
+dfa['isValid'] = dfa['isValid'].apply(lambda x: 'yes' if x else 'no')
+
+# Done
+df = df.join(dfa)
+
+print(df.head())
 print(df.loc[df['isValid'] == 'no'])
